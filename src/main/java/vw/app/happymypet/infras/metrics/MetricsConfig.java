4628a0868480f3292e50fa2350dbd26a273dc137
voyagerwoo <voyager.woo@gmail.com>
2018-10-15 22:26:39 +0900

    Config metrics

--- src/main/java/vw/app/happymypet/infras/metrics/MetricsConfig.java
+++ src/main/java/vw/app/happymypet/infras/metrics/MetricsConfig.java
@@ -1,16 +1,16 @@
 package vw.app.happymypet.infras.metrics;
 
 import com.amazonaws.util.StringUtils;
-import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.cloudwatch.CloudWatchMeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
@@ -24,8 +24,12 @@ public class MetricsConfig {
     @Value("${HOSTNAME:}")
     String hostname;
 
-    @Bean
-    MeterRegistry.Config globalTagConfig() {
+    @Autowired
+    CloudWatchMeterRegistry cloudWatchMeterRegistry;
+
+
+    @PostConstruct
+    void init() {
         String profile = Arrays.stream(env.getActiveProfiles()).collect(Collectors.joining(","));
         if (StringUtils.isNullOrEmpty(profile)) {
             profile = "default";
@@ -34,13 +38,13 @@ public class MetricsConfig {
         log.info("### PROFILE : " + profile);
         log.info("### HOSTNAME : " + hostname);
 
-        if (StringUtils.isNullOrEmpty(hostname))
-            return Metrics.globalRegistry.config()
-                    .commonTags(Collections.singletonList(Tag.of("profiles", profile)));
 
-        return Metrics.globalRegistry.config()
-                .commonTags(Arrays.asList(Tag.of("host", hostname), Tag.of("profiles", profile)));
-
+        if (StringUtils.isNullOrEmpty(hostname)) {
+            cloudWatchMeterRegistry.config().commonTags(Collections.singletonList(Tag.of("profiles", profile)));
+        } else {
+            cloudWatchMeterRegistry.config().commonTags(Arrays.asList(Tag.of("host", hostname), Tag.of("profiles", profile)));
+        }
+        Metrics.addRegistry(cloudWatchMeterRegistry);
     }
 
 }
